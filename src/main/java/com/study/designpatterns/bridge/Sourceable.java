61b02cc8416e99494f8f0c8e653ec53430c22c85
adley-yang <adleyyang.cn@gmail.com>
2014-12-26 15:34:00 +0800

    组合模式（Composite）

---
+++ src/main/java/com/study/designpatterns/bridge/Sourceable.java
@@ -0,0 +1,11 @@
+package com.study.designpatterns.bridge;
+
+/**
+ * Project Name:java
+ * Package Name:com.study.designpatterns.bridge
+ * Date:14-12-26 下午3:08
+ * Copyright (c) 2014, adleyyang.cn@gmail.com All Rights Reserved.
+ */
+public interface Sourceable {
+    public void method();
+}
